589a00c9d8f493445c51250c965fcb3dbb311dec
lazyjerkface <lazyjerkface@users.noreply.github.com>
2017-02-28 22:19:21 -0800

    Add some more examples.

--- main.py
+++ main.py
@@ -5,38 +5,49 @@ app = Flask(__name__)
 
 # Examples can be found at http://flask.pocoo.org/docs/0.12/quickstart/
 
-def build_routes():
-  pages = [
-    {'name': 'Main',
-     'url': url_for('main_page')},
-    {'name': 'hello world',
-     'url': url_for('hello_world')},
-    {'name': 'Username\'s Profile',
-     'url': url_for('show_user_profile', username='Username')},
-    {'name': 'User\'s Profile',
-     'url': url_for('show_user_profile_with_template', name='User')}]
-  return pages
-
-	
-@app.route('/hello')
+# This just prints whatever you want on the page.
+# Exercise 1:
+#   Modify the return value and try inserting HTML on the page.
+#   See the results at http://localhost:5000/
+@app.route('/')
 def hello_world():
     return 'Hello, World!'
 
 
+# You can add variable parts to a URL.
+# Exercise 2:
+#   Print the username in bold if its your name.
+#   See the results at http://localhost:5000/username/<username>
 @app.route('/username/<username>')
 def show_user_profile(username):
   return 'Hello %s' % username
 
  
+# Templates are powerful and dynamic.
+# Exercise 3:
+#   Similar to Exercise 2, print name in bold if its your name.
+#   See the results at http://localhost:5000/user/<name>
 @app.route('/user/')
 @app.route('/user/<name>')
 def show_user_profile_with_template(name=None):
     return render_template('profile.html', name=name)
 
 
-@app.route('/')
+# Template inheritance makes templating even more powerful.
+# main_page() and feature() use a base template (base.html) and extend it.
+# 1) See http://localhost:5000/main and take note of what happens when
+#    you navigate beteween 'Home' and 'Feature'.
+# 2) Take a look at base.html, info.html, and feature.html to understand
+#    how info.html and feature.html extend base.html.
+# Exercise 4:
+#   Create a route to your own page.
+#   HINT: You need to create a function here and modify base.html.
+@app.route('/main')
 def main_page():
-  return render_template('bootstrap.html', pages=build_routes())
+  return render_template('info.html')
 
-
-# Exercise: Create a route to your own page.
+ 
+@app.route('/feature')
+def feature():
+  return render_template('feature.html')
+ 
